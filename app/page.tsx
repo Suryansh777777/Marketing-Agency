56ab154c653a86db1de1063118f351f3d8101a96
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-03 15:50:25 +0530

    Add Trusted component with company logos and marquee animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Trusted from "@/components/Trusted";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Trusted />
     </div>
   );
 };
