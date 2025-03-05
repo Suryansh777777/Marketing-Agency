e34719065e5c5218e178e421e3fff4cda80c1ee9
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-05 16:43:51 +0530

    Add Founders component to display team members with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Founders from "@/components/Founders";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import { Projects } from "@/components/Projects";
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <Trusted />
       <Projects />
+      <Founders />
     </div>
   );
 };
