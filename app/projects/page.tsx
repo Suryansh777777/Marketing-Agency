803efe29e843fad87c08382f9f7855b8cde4a7b6
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2025-03-07 22:55:15 +0530

    Add Projects page with Navbar, ProjectHero, and Footer components

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import ProjectHero from "@/components/ProjectHero";
+
+const Projects = () => {
+  return (
+    <div>
+      <Navbar />
+      <ProjectHero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Projects;
